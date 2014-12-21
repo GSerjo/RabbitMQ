72df624dc4876f536298dc9ed019be545c33b034
GSerjo <sedot8@gmail.com>
2014-12-21 23:08:20 +0300

    + Core * HelloClient added Publish message

---
+++ Source/Core/Extensions/ModelExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace Core.Extensions
+{
+    public static class ModelExtensions
+    {
+        public static void CreateDurableQueue(this IModel model, string queueName)
+        {
+            model.QueueDeclare(queueName, true, false, false, null);
+        }
+
+        public static void CreateDurableDirectExcange(this IModel model, string exchangeName)
+        {
+            model.ExchangeDeclare(exchangeName, ExchangeType.Direct, true);
+        }
+    }
+}
--- Source/HelloClient/Program.cs
+++ Source/HelloClient/Program.cs
@@ -1,11 +1,42 @@
 ﻿using System;
+using System.Text;
+using Core.Extensions;
+using RabbitMQ.Client;
 
 namespace HelloClient
 {
     internal class Program
     {
+        private const string HostName = "localhost";
+        private const string Password = "guest";
+        private const string UserName = "guest";
+        private const string QueueName = "HelloQueue";
+        private const string ExchangeName = "HelloExchange";
+
         private static void Main()
         {
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = HostName,
+                Password = Password,
+                UserName = UserName
+            };
+
+            var connection = connectionFactory.CreateConnection();
+            IModel model = connection.CreateModel();
+
+            model.CreateDurableQueue(QueueName);
+            model.CreateDurableDirectExcange(ExchangeName);
+            model.QueueBind(QueueName, ExchangeName, string.Empty);
+
+            var properties = model.CreateBasicProperties();
+            properties.SetPersistent(true);
+
+            var messageData = Encoding.Default.GetBytes("Test message");
+
+            model.BasicPublish(ExchangeName, string.Empty, properties, messageData);
+
+            Console.WriteLine("Done");
         }
     }
 }

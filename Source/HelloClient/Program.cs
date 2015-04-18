6db175940dea8d6db924edb7f2630341fc74a06e
GSerjo <sedot8@gmail.com>
2015-04-18 07:48:25 +0300

    * Client added request wcf service

--- Source/HelloClient/Program.cs
+++ Source/HelloClient/Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.ServiceModel.Web;
 using Core;
 using Core.Extensions;
 using Core.RabbitMQClients;
 using HelloContracts;
+using Nelibur.ServiceModel.Services;
+using Nelibur.ServiceModel.Services.Default;
+using Nelibur.ServiceModel.Services.Operations;
 using Nelibur.Sword.Extensions;
 using RabbitMQ.Client;
 
 namespace HelloClient
 {
-    internal class Program
+    internal class Program : IPostOneWay<DataRequest>
     {
         private const string ExchangeName = "HelloExchange";
         private const string HostName = "localhost";
@@ -17,14 +21,43 @@ namespace HelloClient
         private const string UserName = "guest";
         private static readonly DataSerializer _dataSerializer = new DataSerializer();
         private static IProducerMQ<Data> _producer;
+        private static WebServiceHost _service;
+        private RabbitMQClient _rabbitClient;
+
+        public void PostOneWay(DataRequest request)
+        {
+            var data = new Data
+            {
+                Id = request.Id
+            };
+            _producer.EnqueuePersistent(data);
+        }
 
         private static void Main()
         {
-            //            Send1();
-            Send2();
+            new Program().Run();
         }
 
-        private static void Send1()
+        private void Run()
+        {
+            InitialiseProducer();
+
+            _service = new WebServiceHost(typeof(JsonServicePerCall));
+            _service.Open();
+
+            NeliburRestService.Configure(x => x.Bind<DataRequest, Program>());
+
+            //            Send1();
+            //            Send2();
+
+            Console.WriteLine("Press Any Key to send a message");
+            Console.ReadKey();
+
+            _rabbitClient.SafeDispose();
+            _service.Close();
+        }
+
+        private void Send1()
         {
             var connectionFactory = new ConnectionFactory
             {
@@ -54,16 +87,9 @@ namespace HelloClient
             model.Close();
         }
 
-        private static void Send2()
+        private void Send2()
         {
-            RabbitMQClient rabbitClient = RabbitMQClient.Configure(x =>
-            {
-                x.HostName = HostName;
-                x.Password = Password;
-                x.UserName = UserName;
-            }).Create();
-
-            _producer = rabbitClient.GetProducer<Data>(ExchangeName, QueueName, _dataSerializer.ToProto);
+            InitialiseProducer();
 
             while (true)
             {
@@ -78,13 +104,25 @@ namespace HelloClient
             }
         }
 
-        private static void SendMessage(int messageIndex)
+        private void InitialiseProducer()
+        {
+            _rabbitClient = RabbitMQClient.Configure(x =>
+            {
+                x.HostName = HostName;
+                x.Password = Password;
+                x.UserName = UserName;
+            }).Create();
+
+            _producer = _rabbitClient.GetProducer<Data>(ExchangeName, QueueName, _dataSerializer.ToProto);
+        }
+
+        private void SendMessage(int messageIndex)
         {
             _producer.EnqueuePersistent(new Data { Id = Guid.NewGuid() });
             Console.WriteLine("Message {0}, was sent", messageIndex);
         }
 
-        private static void SendMessage(IModel model, int messageIndex)
+        private void SendMessage(IModel model, int messageIndex)
         {
             var data = new Data
             {
---
+++ Source/HelloContracts/DataRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace HelloContracts
+{
+    [DataContract]
+    public sealed class DataRequest
+    {
+        [DataMember]
+        public Guid Id { get; set; }
+    }
+}
